a05b30b191196b207869858e7029e7f5c162e5a0
Pramod <pramod4342@gmail.com>
2026-05-29 15:37:50 +0530

    post req

--- main2.py
+++ main2.py
@@ -1,18 +1,36 @@
 from fastapi import FastAPI
+
 from pydantic import BaseModel
 
+
+class Products(BaseModel):
+    id: int
+    name: str
+    description: str
+
+
+products = [
+    Products(id=1, name="Product 1", description="Description 1"),
+    Products(id=2, name="Product 2", description="Description 2"),
+    Products(id=3, name="Product 3", description="Description 3"),
+]
 app = FastAPI()
 
 
-class UserMessage(BaseModel):
-    message: str
+@app.get("/products")
+def get_products():
+    return products
 
 
-@app.get("/")
-def home():
-    return {"status": "running"}
+@app.get("/product/{id}")
+def get_product(id: int):
+    for pr in products:
+        if pr.id == id:
+            return pr
+    return "product not found"
 
 
-@app.post("/chat")
-def chat(data: UserMessage):
-    return {"reply": f"AI says: {data.message}"}
+@app.post("/product")
+def post_product(product: Products):
+    products.append(product)
+    return product
